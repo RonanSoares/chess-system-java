fb19211a9cdc9508c0a7868d19eaa0167dec41b5
Ronan Soares <ronanp2@hotmail.com>
2023-09-27 09:55:31 -0300

    Criado classe Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import jogoDeTabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Posicao posicao = new Posicao(3, 5);
+		System.out.println(posicao);
+		
+		sc.close();
 
 	}
 
